db55461d8f738a65876d0e56ca2f5e72ef703321
Anomig <goyvaerts.naomi@hotmail.com>
2024-11-22 14:16:46 +0100

    feat: setup basic THREEjs

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import gsap from 'gsap'
+import {DRACOLoader} from 'three/examples/jsm/Addons.js'
+import * as dat from 'dat.gui'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-setupCounter(document.querySelector('#counter'))
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnomationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+//orbit controls ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+//light //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const light = new THREE.AmbientLight(0xffffff, 1);
+scene.add(light);
+
+//camera /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+camera.position.z = 5;
+
+//Animation Loop /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function animate() {
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+animate();
