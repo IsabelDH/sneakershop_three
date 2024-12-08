c0d4abd5b690f2f4f5d69b9540843996a7693f1f
IsabelDH <isabel.de.haes@hotmail.com>
2024-12-08 20:46:28 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: 'index.html',  // index.html is nu in de root
-        configurator: 'configurator.html',  // configurator.html is ook in de root
+        index: 'index.html', // Hoofdpagina
+        configurator: 'configurator.html', // Andere pagina
       },
     },
   },
