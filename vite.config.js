eb0ac30dc2f952024dac40caacf1901455d0fd32
IsabelDH <isabel.de.haes@hotmail.com>
2024-12-08 20:29:28 +0100

    fix 3

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
+    outDir: 'dist',  // Zorg ervoor dat de output naar de dist-map gaat
     rollupOptions: {
       input: {
-        index: 'index.html',       // De hoofdpagina
-        configurator: 'configurator.html',  // Je tweede pagina
+        index: 'index.html',
+        configurator: 'configurator.html',  // Als je configurator.html hebt
       },
     },
   },
