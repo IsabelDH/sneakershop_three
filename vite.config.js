83d2b3a35f9da219e5ecd0d822ef07c27ebe8449
Anomig <goyvaerts.naomi@hotmail.com>
2024-12-08 19:14:40 +0100

    fix: nieuwe poging om vercel live te krijgen door met public map te werken en src map

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir: 'dist', // Dit is waar de gebundelde bestanden komen
     rollupOptions: {
-      input: '/index.html', // Dit is de hoofdingang van je app
+      input: {
+        main: 'index.html',
+        page2: 'public/configurator.html',
+      },
     },
   },
 });
